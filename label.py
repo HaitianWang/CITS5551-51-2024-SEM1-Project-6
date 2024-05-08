7a4d80b3810a448f3bcf2c42eb7b8cff40df91ce
Ryan Chen <69900861+RyanChen88@users.noreply.github.com>
2024-05-08 17:07:11 +0800

    Update label.py

--- label.py
+++ label.py
@@ -1,5 +1,6 @@
 import rasterio
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 red_path = ""  # 红色地址
@@ -17,25 +18,39 @@ with rasterio.open(nir_path) as nir_ds:
 # 计算NDVI
 ndvi = (nir_array - red_array) / (nir_array + red_array + 1e-8)
 
-ndvi_mapped = np.where(ndvi > 0.6, 0, np.where(ndvi >= 0.3, 1, 2))
+ndvi_mapped = np.where(ndvi > 0.54, 0, np.where(ndvi >= 0.46, 1, 2))
 
-# 定义颜色映射
-colors = {0: 'red', 1: 'green', 2: 'blue'}
 
-# 创建彩色图像
-ndvi_color_mapped = np.zeros((ndvi_mapped.shape[0], ndvi_mapped.shape[1], 3), dtype=np.uint8)
-# print(ndvi_color_mapped[2000:3000, 1000:1500])
-print("ndvi_mapped形状:", ndvi_mapped.shape)
-print("ndvi_color_mapped形状:", ndvi_color_mapped.shape)
+# 将NDVI映射矩阵转换为DataFrame
+df = pd.DataFrame(ndvi_mapped)
 
-# 对每个值着色
-for i, color in colors.items():
-    # 获取颜色的RGB值
-    color_rgb = np.array(plt.cm.colors.to_rgba(color)[:3]) * 255
-    # 根据颜色和值进行索引和赋值
-    ndvi_color_mapped[ndvi_mapped == i] = color_rgb
+# 定义保存路径
+csv_path = "ndvi_label.csv"
 
-# 显示彩色图像
-plt.imshow(ndvi_color_mapped)
-plt.axis('off')
-plt.show()
+# 保存为CSV文件
+df.to_csv(csv_path, index=False, header=False)
+
+# # 定义颜色映射
+# colors = {0: 'red', 1: 'green', 2: 'blue'}
+#
+# # 创建彩色图像
+# ndvi_color_mapped = np.zeros((ndvi_mapped.shape[0], ndvi_mapped.shape[1], 3), dtype=np.uint8)
+#
+# # 对每个值着色
+# for i, color in colors.items():
+#     # 获取颜色的RGB值
+#     color_rgb = np.array(plt.cm.colors.to_rgba(color)[:3]) * 255
+#     # 根据颜色和值进行索引和赋值
+#     ndvi_color_mapped[ndvi_mapped == i] = color_rgb
+#
+# ndvi_color_path = "label_0.6_0.46.png"
+#
+# # 显示彩色图像
+# plt.imshow(ndvi_color_mapped)
+# plt.axis('off')
+#
+# # 保存图像到文件
+# plt.savefig(ndvi_color_path, bbox_inches='tight', pad_inches=0, dpi=96)
+#
+# # 关闭图形窗口
+# plt.close()
