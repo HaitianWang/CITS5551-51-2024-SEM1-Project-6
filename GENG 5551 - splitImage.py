7115541f51829ebbe743117fa4ecee6a1e44064d
Haitian Wang <48538377+HaitianWang@users.noreply.github.com>
2024-05-23 16:03:18 +0800

    Update GENG 5551 - splitImage.py

--- GENG 5551 - splitImage.py
+++ GENG 5551 - splitImage.py
@@ -6,22 +6,22 @@ pic_path = 'C:\\UWA\\GENG 5551\\RGB Original\\2020 07 30 Kondinin E2 RGB.tif'
 
 if pic_path is None:
     raise ValueError('Failed to read image.')
-pic_target = 'C:\\UWA\\GENG 5551\\2020 RGB Split Jpg' # 分割后的图片保存的文件夹
-if not os.path.exists(pic_target):  #判断是否存在文件夹如果不存在则创建为文件夹
+pic_target = 'C:\\UWA\\GENG 5551\\2020 RGB Split Jpg' 
+if not os.path.exists(pic_target):
     os.makedirs(pic_target)
-#要分割后的尺寸
+
 cut_width = 256
 cut_length = 256
-# 读取要分割的图片，以及其尺寸等数据
+
 picture = cv2.imread(pic_path)
 #picture = cv2.imread("2020 07 30 Kondinin E2 RGB.tif")
 (width, length, depth) = picture.shape
-# 预处理生成0矩阵
+
 pic = np.zeros((cut_width, cut_length, depth))
-# 计算可以划分的横纵的个数
+
 num_width = int(width / cut_width)
 num_length = int(length / cut_length)
-# for循环迭代生成
+
 for i in range(0, num_width):
     for j in range(0, num_length):
         pic = picture[i*cut_width : (i+1)*cut_width, j*cut_length : (j+1)*cut_length, :]      
